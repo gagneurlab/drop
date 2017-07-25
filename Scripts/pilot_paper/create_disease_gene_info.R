5cf4b450f2b27e595cdf80494b1420fc5e886292
Daniel M Bader <baderda@in.tum.de>
2017-07-25 17:39:39 +0200

    first version of meta disease gene table

---
+++ Scripts/pilot_paper/create_disease_gene_info.R
@@ -0,0 +1,162 @@
+#'---
+#' title: Combine info on disease genes and solved patients
+#' author: Daniel Bader
+#' wb:
+#'   input: 
+#'   output: 
+#' output: 
+#'   html_document:
+#'     toc_float: yes
+#'     toc: yes
+#'---
+#'
+
+
+
+#+ echo=F
+source("src/r/config.R")
+
+#' 
+#' # Collect disease genes
+#' 
+dir_gene_info <- '/s/project/mitoMultiOmics/raw_data/gene_info/'
+dir_gene_info
+
+file_meta_disease_gene <- file.path(dir_gene_info, 'meta_disease_genes.tsv')
+
+#' 
+#' ## File: list of genes by disease
+#' 
+disgene_dt <- as.data.table(read.delim(
+    file.path(dir_gene_info, 'disease_genes.tsv'), 
+    na.strings = '', 
+    stringsAsFactors = F
+    ))
+#' * clean up disease names
+setnames(disgene_dt, c('MITO', 'NBIA', 'other'))
+
+#' * melt table
+disgene_dt <- unique(melt(
+    disgene_dt, 
+    measure.vars=names(disgene_dt), 
+    variable.name='DISEASE', 
+    value.name='HGNC_GENE_NAME'
+    ))
+disgene_dt <- disgene_dt[!is.na(HGNC_GENE_NAME)][order(HGNC_GENE_NAME)]
+
+#' * set SPG7 to MITO
+disgene_dt[HGNC_GENE_NAME=='SPG7', DISEASE:='MITO']
+
+#+
+head(disgene_dt)
+dim(disgene_dt)
+
+
+#' 
+#' ## File: paper disease gene list
+#' 
+paper_dt <- as.data.table(read.delim(
+    file.path(dir_gene_info, 'kremer_bader_2016_biorxiv_mitochondrial_disease_genes.tsv'), 
+    na.strings = '', 
+    stringsAsFactors = F
+))
+setnames(paper_dt, c('HGNC_GENE_NAME', 'FULL_GENE_NAME', 'MIM_NUMBER', 'OMIM_LINK', 'ENTREZ_GENE_ID'))
+paper_dt[is.na(MIM_NUMBER), OMIM_LINK:=NA]
+paper_dt[, DISEASE:='MITO']
+#+
+head(paper_dt)
+dim(paper_dt)
+
+
+#' ### Merge
+#' 
+meta_disease_gene_dt <- merge(disgene_dt, paper_dt, all=T)
+
+# remove spaces
+meta_disease_gene_dt[, HGNC_GENE_NAME:=gsub(' ','',HGNC_GENE_NAME)]
+# remove trailing spaces
+meta_disease_gene_dt <- meta_disease_gene_dt[,lapply(.SD, function(j) gsub(' +$', '', j))]
+
+
+
+#' 
+#' ## File: manually curated list by Prokisch and Mayr
+#' 
+create_clean_prokisch_mayr_table()
+#' 
+prokisch_mayr_dt <- fread(
+    file.path(dir_gene_info, 'mitochondrial_disorder_genes_prokisch_mayr_cleaned.tsv')
+)
+#+
+sort(names(prokisch_mayr_dt))
+
+
+#'
+#' ### Merge
+#'
+dt <- merge(meta_disease_gene_dt, prokisch_mayr_dt, all=T, by='HGNC_GENE_NAME')
+dim(dt)
+
+#' * drop full gene names; can be inferred later, if needed
+dt[,FULL_GENE_NAME.x:=NULL]
+dt[,FULL_GENE_NAME.y:=NULL]
+
+#' * fill NAs in duplicated columns bi-directional
+icols <- c('HGNC_GENE_NAME', 'DISEASE.x', 'DISEASE.y', 'ENTREZ_GENE_ID.x', 'ENTREZ_GENE_ID.y')
+merged_cols <- gsub('\\.x', '', grep('\\.x', names(dt), value=T))
+
+for(coln in merged_cols){
+    dt[is.na(get(paste0(coln,'.x'))) | is.na(get(paste0(coln,'.y'))), 
+        eval(coln) := na.omit(c(get(paste0(coln,'.x')), get(paste0(coln,'.y')))), 
+        by=HGNC_GENE_NAME
+        ]    
+}
+
+
+#' * check duplicated columns for mismatches
+dt[DISEASE.x != DISEASE.y, icols, with=F]
+#' DISEASE --> keep coln.x
+dt[is.na(DISEASE), DISEASE:= DISEASE.x]
+#' 
+dt[ENTREZ_GENE_ID.x != ENTREZ_GENE_ID.y, c(icols,merged_cols), with=F]
+#' ENTREZ_GENE_ID --> all fine
+dt[is.na(ENTREZ_GENE_ID), ENTREZ_GENE_ID:= ENTREZ_GENE_ID.x]
+#' 
+#' * remove duplicated columns
+dupl_coln <- grep('\\.[xy]', names(dt), value=T)
+dt[,c(dupl_coln):=NULL]
+
+#' MIM numbers
+#' 
+#' * add single MIM_NUMBERS to list column, where list is NA
+dt[is.na(MIM_NUMBERS) &  !is.na(MIM_NUMBER), MIM_NUMBERS:=MIM_NUMBER]
+#' * check single number is always contained in list
+#' 
+#+
+# make list
+dt[, list_mim:=strsplit(MIM_NUMBERS, ',')]
+# annotate length
+dt[, num_mim_numbers:=length(list_mim[[1]]), by=HGNC_GENE_NAME]
+# compute matching
+dt[, mimX_in_mimY:=MIM_NUMBER %in% unlist(list_mim), by=HGNC_GENE_NAME]
+
+#+
+dt[!is.na(MIM_NUMBER) & mimX_in_mimY==F, .(HGNC_GENE_NAME, MIM_NUMBER, list_mim)]
+#' --> all single MIM_NUMBERS are contained in the list column
+#' 
+#' * remove helper columns
+dt[,c('MIM_NUMBER', 'list_mim', 'num_mim_numbers', 'mimX_in_mimY'):=NULL]
+
+
+#' Final merged columns
+meta_disease_gene_dt <- unique(dt)
+write_tsv(meta_disease_gene_dt, file=file_meta_disease_gene)
+
+#+ results='hold'
+dim(meta_disease_gene_dt)
+meta_disease_gene_dt[,.N, DISEASE]
+sort(names(meta_disease_gene_dt))
+
+
+
+#' ## FILE: mito associated genes
