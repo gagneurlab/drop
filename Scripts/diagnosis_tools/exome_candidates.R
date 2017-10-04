f535dd3e72f99245bca2043baccff3d95a45a5d0
Daniel M Bader <baderda@in.tum.de>
2017-10-04 17:43:10 +0200

    updated signif number colns

--- Scripts/diagnosis_tools/exome_candidates.R
+++ Scripts/diagnosis_tools/exome_candidates.R
@@ -19,7 +19,6 @@ GENE_ANNO <- fread(file_disease_gene_anno, na.strings=c('NA',''))
 exome_candy <- readRDS(file_exome_candy)
 
 
-
 #+ echo=F
 exome_candy <- exome_candy[order(FIBROBLAST_ID, chr, pos)]
 
@@ -47,17 +46,28 @@ columns_to_show <- c(
     'ref',
     'alt'
 )
-exome_candy <- exome_candy[, ..columns_to_show]
 
+# merge exome and disease gene info
+exome_display_dt <- merge(
+    GENE_ANNO[,.(HGNC_GENE_NAME, MIM_NUMBERS, DISEASE)], 
+    exome_candy[, ..columns_to_show], 
+    by.y='hgncid', 
+    by.x='HGNC_GENE_NAME', 
+    all.y=T
+)
 
+# round columns with numbers
+columns_signif <- c('var_size', "sift1", "pph1", 'exacmaf', 'sample_freq')
+for(j in columns_signif){
+    exome_display_dt[, c(j):= list(signif(get(j), digits = 2))]
+}
 
 # renaming
-setnames(exome_candy, 'mtype', 'variant type')
-setnames(exome_candy, 'mstype', 'variant effect')
-setnames(exome_candy, 'hgncid', 'gene ID')
-setnames(exome_candy, 'pph1', 'polyphen score')
-setnames(exome_candy, 'sift1', 'sift score')
-setnames(exome_candy, 'exacmaf', 'ExAC MAF')
+setnames(exome_display_dt, 'mtype', 'variant type')
+setnames(exome_display_dt, 'mstype', 'variant effect')
+setnames(exome_display_dt, 'pph1', 'polyphen score')
+setnames(exome_display_dt, 'sift1', 'sift score')
+setnames(exome_display_dt, 'exacmaf', 'ExAC MAF')
 
 
 
@@ -70,9 +80,8 @@ setnames(exome_candy, 'exacmaf', 'ExAC MAF')
 #' 
 
 #+ echo=F
-#head(exome_candy)
-
-DT::datatable(exome_candy, filter='top', rownames = FALSE)
+#head(exome_display_dt)
+DT::datatable(exome_display_dt, filter='top', rownames = FALSE)
 
 
 
