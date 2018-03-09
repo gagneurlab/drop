2db3e24cce0b1b6dad53b49684c9304c934dcd84
Vicente <yepez@in.tum.de>
2018-03-09 17:01:31 +0100

    create_mito_carta_genes added

---
+++ Scripts/pilot_paper/create_mito_carta_genes.R
@@ -0,0 +1,42 @@
+#'---
+#' title: MitoCarta genes
+#' author: Vicente Yépez
+#' wb:
+#'   input: "/s/project/mitoMultiOmics/raw_data/gene_info/Human.MitoCarta2.0xls"
+#'   output: 
+#' output: 
+#'   html_document
+#'---
+#'
+
+#+ echo=F
+library(data.table)
+library(DT)
+library(readxl)
+
+# Input
+dir_gene_info <- '/s/project/mitoMultiOmics/raw_data/gene_info/'
+file_excel_mito_carta <- file.path(dir_gene_info, 'Human.MitoCarta2.0.xls')
+mito_carta <- read_excel(file_excel_mito_carta, sheet = "A Human MitoCarta2.0") %>% as.data.table
+# Output
+file_out_mito_carta <- file.path(dir_gene_info, 'mito_carta_genes.tsv')
+
+# Important cols
+cols <- c(
+    'Symbol', 
+    'EnsemblGeneID',
+    'Description',
+    'Synonyms',
+    'hg19_Chromosome',
+    'Tissues')
+
+mito_carta <- mito_carta[, cols, with=F]
+
+setnames(mito_carta, "Symbol", "HGNC_GENE_NAME")
+setnames(mito_carta, "EnsemblGeneID", "ENSEMBL_ID")
+setnames(mito_carta, "hg19_Chromosome", "Chromosome")
+names(mito_carta) <- toupper(names(mito_carta))
+
+write.table(mito_carta, file = file_out_mito_carta, quote = F, sep = "\t", row.names = F)
+
+
