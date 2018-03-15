219a64eb0c18fa2c9bbf42d26b3e69078110a5fa
Vicente <yepez@in.tum.de>
2018-03-15 18:20:39 +0100

    new count scripts added

--- src/r/processing/get_counts_batch0.R
+++ src/r/processing/get_counts_batch0.R
@@ -1,10 +1,16 @@
 suppressPackageStartupMessages(source("src/r/config.R"))
-exons_op = readRDS("resources/exons_op.Rds")
-bf <- substr(scan("resources/201711_nadel_rna_seq_samples.txt", what = character()), 3, 100)
-samples <- vapply(strsplit(bf, "/"), "[", "", 1)
+exons_en = readRDS("resources/exons_en.Rds")
 
-bamfiles <- BamFileList(file.path(RAWDIR, "helmholtz", bf), yieldSize=2000000)
-names(bamfiles) <- samples
+old_samples = scan("resources/kremer_bader_rna_ids.txt", what = character())
+
+RAWDATADIR <- "/s/project/mitoMultiOmics/raw_data/helmholtz"
+
+bamfiles = BamFileList(
+    sapply(old_samples, get_helmholtz_file, 'rna','BAM.STAR'), # function to get BAM file path
+    obeyQname=FALSE, 
+    yieldSize= 1e6
+)
+names(bamfiles) = old_samples
 
 my_bpparam = register_bplapply_for_clustering(slurm = F, workers = 40, threads = 2, memory = 8000, jobname = "count_rna")
 
@@ -12,7 +18,7 @@ starttime= Sys.time()
 
 library(GenomicAlignments)
 se = summarizeOverlaps(
-    exons_op,
+    exons_en,
     bamfiles,
     mode = 'IntersectionStrict',
     singleEnd = FALSE,
@@ -23,7 +29,7 @@ se = summarizeOverlaps(
 )
 message('Processed all fibros in: ', format(Sys.time()- starttime))
 
-saveRDS(se, "/s/project/genetic_diagnosis/processed_data/Rds/se_batch3.Rds")
+saveRDS(se, file.path(PROC_DATA, "se_batch0.Rds"))
 message('counting done')
 
 
@@ -35,12 +41,12 @@ se_ex = merge(se_ex, exons_gene_dt, by = "exon_id")
 
 
 # Aggregate exon data by gene and turn into a matrix
-se_gene = se_ex[, lapply(.SD, sum), by = gene_name, .SDcols = colnames(assay(se))]
+se_gene = se_ex[, lapply(.SD, sum, na.rm = T), by = gene_name, .SDcols = colnames(assay(se))]
 se_matrix = as.matrix(se_gene[, colnames(assay(se)), with = F])
 row.names(se_matrix) = se_gene$gene_name
 dim(se_matrix)   # 54,356 rows
 se_matrix = se_matrix[rowSums2(se_matrix) > 0, ]
 dim(se_matrix)   # ~ 30,000 rows
 
-write.table(se_matrix, file.path(PROC_DATA, "rna_batch3_strand_specific.txt"), quote = F, col.names = T, row.names = T)
+write.table(se_matrix, file.path(PROC_DATA, "rna_batch0_new_annotation.txt"), quote = F, col.names = T, row.names = T)
 message('matrix done')
