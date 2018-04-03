ebaafe4412da643f5b77456c722e9227d8e82a2a
Agne Matuseviciute <matusevi@ouga04.i12g.informatik.tu-muenchen.de>
2018-04-03 18:40:42 +0200

    strand spec/not str spec

--- Scripts/diagnosis_tools/rna_candidates_outrider.R
+++ Scripts/diagnosis_tools/rna_candidates_outrider.R
@@ -43,23 +43,27 @@ b1<- read.table(file_rna_b1, sep=" ", check.names=F, header=T, row.names=1)
 b2<- read.table(file_rna_b2, sep=" ", check.names=F, header=T, row.names=1)
 b3<- read.table(file_rna_b3, sep=" ", check.names=F, header=T, row.names=1)
 
-#' # merge only new batches
+#' #not strand specific
 #+ 
-b12 <- merge(b1, b2, by=0, all=FALSE)
-b_12 <- b12[,-1]
-rownames(b_12) <-  b12[,1]
-b123 <- merge(b_12, b3, by=0, all=FALSE)
-b_123 <- b123[,-1]
-m <- match(b123[,1], hgnc$transcript_name)
-rownames(b_123) <- ensembl$gene_id[m]
+b01 <- merge(b0, b1, by=0, all=FALSE)
+b_0_1 <- b01[,-1]
+m <- match(b01[,1], hgnc$transcript_name)
+rownames(b_0_1) <- ensembl$gene_id[m]
 
-#' # merge all batches
+#' #strand specific
 #+ 
-m <- match(rownames(b0), hgnc$transcript_name)
-rownames(b0) <- ensembl$gene_id[m]
-b0123 <- merge(b0, b_123, by=0, all=FALSE)
-b_0123 <- b0123[,-1]
-rownames(b_0123) <-  b0123[,1]             
+b23 <- merge(b2, b3, by=0, all=FALSE)
+b_2_3 <- b23[,-1]
+m <- match(b23[,1], hgnc$transcript_name)
+rownames(b_2_3) <- ensembl$gene_id[m]
+
+
+#' #combine all batches
+#+ 
+comb <- merge(b_0_1, b_2_3, by=0, all=FALSE)
+b_0123 <- comb[,-1]
+rownames(b_0123) <- comb[,1]
+            
 
 #' # run OUTRIDER, all batches
 #+ 
@@ -139,6 +143,8 @@ DT::datatable(
     rownames = FALSE
 )
 
+saveRDS(few_outliers_per_sample, "/s/project/genetic_diagnosis/processed_results/all_outr.Rds")
+
 disp_not_solved <-display[display$SOLVED!="TRUE",]
 
 #'  
@@ -152,51 +158,63 @@ DT::datatable(
     rownames = FALSE
 )
 
-#' # run OUTRIDER, only new batches
+###############################################################################
+
+#' # run OUTRIDER, not strand specific 
 #+ 
-ods_123 <- OUTRIDER::OutriderDataSet(countData=b_123)
-ods_123 <- OUTRIDER::filterExpression(ods_123, gtfFile=gtf,
+ods_01 <- OUTRIDER::OutriderDataSet(countData=b_0_1)
+ods_01 <- OUTRIDER::filterExpression(ods_01, gtfFile=gtf,
          filterGenes=FALSE, savefpkm=TRUE)
-OUTRIDER::plotFPKM(ods_123) #set filter genes to FALSE before
-ods_123 <- OUTRIDER::filterExpression(ods_123, gtfFile=gtf,
+OUTRIDER::plotFPKM(ods_01) #set filter genes to FALSE before
+ods_01 <- OUTRIDER::filterExpression(ods_01, gtfFile=gtf,
          filterGenes=TRUE, savefpkm=TRUE)
-ods_123 <- OUTRIDER::estimateSizeFactors(ods_123)
-ods_123 <- OUTRIDER::autoCorrect(ods_123)
-#OUTRIDER::plotCountCorHeatmap(ods_123, normalized = TRUE, nCluster = 0, dendrogram="row", names="none", margins=c(1,1))
-ods_123 <- OUTRIDER::fit(ods_123)
-ods_123 <- OUTRIDER::computePvalues(ods_123, alternative="two.sided")
-ods_123 <- OUTRIDER::computeZscores(ods_123)
-#res_all_0.1 <- OUTRIDER::results(ods_all, padj=0.1)
-res_123_0.05 <- OUTRIDER::results(ods_123)
-OUTRIDER::plotAberrantPerSample(ods_123, padj=0.05)
+ods_01 <- OUTRIDER::estimateSizeFactors(ods_01)
+ods_01 <- OUTRIDER::autoCorrect(ods_01)
+#OUTRIDER::plotCountCorHeatmap(ods_01, normalized = TRUE, nCluster = 0, dendrogram="row", names="none", margins=c(1,1,1,1))
+ods_01 <- OUTRIDER::fit(ods_01)
+ods_01 <- OUTRIDER::computePvalues(ods_01, alternative="two.sided")
+ods_01 <- OUTRIDER::computeZscores(ods_01)
+#res_01_0.1 <- OUTRIDER::results(ods_01, padj=0.1)
+res_01_0.05 <- OUTRIDER::results(ods_01)
+OUTRIDER::plotAberrantPerSample(ods_01, padj=0.05)
 
-
-m <- match(res_123_0.05$geneID, ensembl$gene_id)
-res_123_0.05$hgnc <- hgnc$transcript_name[m]
-
-display_dt_123 <- merge(
+#' # prepare result tables, 01 batches. Adding OMIM and sample annotation info
+#+ 
+m <- match(res_01_0.05$geneID, ensembl$gene_id)
+res_01_0.05$hgnc <- hgnc$transcript_name[m]
+display_dt_01 <- merge(
     GENE_ANNO[,.(HGNC_GENE_NAME, DISEASE, SUB_CATEGORY,FUNCTION, MIM_NUMBERS)], 
-    res_123_0.05, 
+    res_01_0.05,
     by.x='HGNC_GENE_NAME', 
     by.y='hgnc', 
     all.y=T
 )
-
 #omim
-disp_123 <- left_join(display_dt_123, unique(omim_dt[SYMBOL != "", .(SYMBOL, GMIM, PINH, PMIM)]), 
+disp_01 <- left_join(display_dt_01, unique(omim_dt[SYMBOL != "", .(SYMBOL, GMIM, PINH, PMIM)]),
                   by = c("HGNC_GENE_NAME" = "SYMBOL")) %>% as.data.table()
 #sample anno
-display_123 <- left_join(disp_123, sample_anno[, .(RNA_ID, KNOWN_MUTATION, CANDIDATE_GENE, COMMENT, SOLVED, 
+display_01 <- left_join(disp_01, sample_anno[, .(RNA_ID, KNOWN_MUTATION, CANDIDATE_GENE, COMMENT, SOLVED,
                                BIOCHEMICAL_DEFECT,  DISEASE, RNA_PERSON)], #CLINICAL_SYMPTOMS,
                   by = c("sampleID" = "RNA_ID")) %>% as.data.table
-setnames(display_123, old = "sampleID", "RNA_ID")
-setnames(display_123, old = "DISEASE.x", "Disease_info")
-setnames(display_123, old = "DISEASE.y", "Disease_sample_anno")
+setnames(display_01, old = "sampleID", "RNA_ID")
+setnames(display_01, old = "DISEASE.x", "Disease_info")
+setnames(display_01, old = "DISEASE.y", "Disease_sample_anno")
 
 
-few_outliers_per_sample_123 <- display_123[display_123$AberrantBySample <length(ods_all)*0.001,]
+#'  
+#'  Full result table, after running OUTRIDER, absolute Z-score > 3, adjusted P-value < 0.05
+#' 
+#+ echo=F
+#head(display_dt_01)
+DT::datatable(
+    display_01,
+    filter='top', 
+    rownames = FALSE
+)
 
-discarded_123 <- display_123[display_123$AberrantBySample > length(ods_123)*0.001,c("RNA_ID","RNA_PERSON")]
+few_outliers_per_sample_01 <- display_01[display_01$AberrantBySample <length(ods_01)*0.001,]
+
+discarded_01 <- display_01[display_01$AberrantBySample > length(ods_01)*0.001,c("RNA_ID","RNA_PERSON")]
 
 #'  
 #'  Discarded samples with more than 0.1% outliers: 
@@ -204,19 +222,130 @@ discarded_123 <- display_123[display_123$AberrantBySample > length(ods_123)*0.00
 #+ echo=F
 #head(discarded)
 DT::datatable(
-    discarded_123, 
+    discarded_01,
     filter='top', 
     rownames = FALSE
 )
 
 #'  
-#'  Result table, for batches 1, 2, 3, Samples with less than 0.1% of outliers 
+#'  Result table, Samples with less than 0.1% of outliers 
 #' 
 #+ echo=F
-#head(display_dt)
+#head(few_outliers_per_sample_01)
 DT::datatable(
-    few_outliers_per_sample_123, 
+    few_outliers_per_sample_01,
     filter='top', 
     rownames = FALSE
 )
+
+saveRDS(few_outliers_per_sample_01, "/s/project/genetic_diagnosis/processed_results/01_outr.Rds")
+
+disp_not_solved_01 <-display_01[display_01$SOLVED!="TRUE",]
+
+#'  
+#'  Samples, which are not solved yet
+#' 
+#+ echo=F
+#head(disp_not_solved_01)
+DT::datatable(
+    disp_not_solved_01,
+    filter='top', 
+    rownames = FALSE
+)
+
+###############################################################################
+
+#' # run OUTRIDER, strand specific
+#+ 
+ods_23 <- OUTRIDER::OutriderDataSet(countData=b_2_3)
+ods_23 <- OUTRIDER::filterExpression(ods_23, gtfFile=gtf,
+         filterGenes=FALSE, savefpkm=TRUE)
+OUTRIDER::plotFPKM(ods_23) #set filter genes to FALSE before
+ods_23 <- OUTRIDER::filterExpression(ods_23, gtfFile=gtf,
+         filterGenes=TRUE, savefpkm=TRUE)
+ods_23 <- OUTRIDER::estimateSizeFactors(ods_23)
+ods_23 <- OUTRIDER::autoCorrect(ods_23)
+#OUTRIDER::plotCountCorHeatmap(ods_23, normalized = TRUE, nCluster = 0, dendrogram="row", names="none", margins=c(1,1))
+ods_23 <- OUTRIDER::fit(ods_23)
+ods_23 <- OUTRIDER::computePvalues(ods_23, alternative="two.sided")
+ods_23 <- OUTRIDER::computeZscores(ods_23)
+#res_23_0.1 <- OUTRIDER::results(ods_23, padj=0.1)
+res_23_0.05 <- OUTRIDER::results(ods_23)
+OUTRIDER::plotAberrantPerSample(ods_23, padj=0.05)
+
+#' # prepare result tables, 23 batches. Adding OMIM and sample annotation info
+#+ 
+m <- match(res_23_0.05$geneID, ensembl$gene_id)
+res_23_0.05$hgnc <- hgnc$transcript_name[m]
+display_dt_23 <- merge(
+    GENE_ANNO[,.(HGNC_GENE_NAME, DISEASE, SUB_CATEGORY,FUNCTION, MIM_NUMBERS)], 
+    res_23_0.05,
+    by.x='HGNC_GENE_NAME', 
+    by.y='hgnc', 
+    all.y=T
+)
+#omim
+disp_23 <- left_join(display_dt_23, unique(omim_dt[SYMBOL != "", .(SYMBOL, GMIM, PINH, PMIM)]),
+                  by = c("HGNC_GENE_NAME" = "SYMBOL")) %>% as.data.table()
+#sample anno
+display_23 <- left_join(disp_23, sample_anno[, .(RNA_ID, KNOWN_MUTATION, CANDIDATE_GENE, COMMENT, SOLVED,
+                               BIOCHEMICAL_DEFECT,  DISEASE, RNA_PERSON)], #CLINICAL_SYMPTOMS,
+                  by = c("sampleID" = "RNA_ID")) %>% as.data.table
+setnames(display_23, old = "sampleID", "RNA_ID")
+setnames(display_23, old = "DISEASE.x", "Disease_info")
+setnames(display_23, old = "DISEASE.y", "Disease_sample_anno")
+
+
+#'  
+#'  Full result table, after running OUTRIDER, absolute Z-score > 3, adjusted P-value < 0.05
+#' 
+#+ echo=F
+#head(display_dt_23)
+DT::datatable(
+    display_23,
+    filter='top', 
+    rownames = FALSE
+)
+
+few_outliers_per_sample_23 <- display_23[display_23$AberrantBySample <length(ods_23)*0.001,]
+
+discarded_23 <- display_23[display_23$AberrantBySample > length(ods_23)*0.001,c("RNA_ID","RNA_PERSON")]
+
+#'  
+#'  Discarded samples with more than 0.1% outliers: 
+#' 
+#+ echo=F
+#head(discarded_23)
+DT::datatable(
+    discarded_23,
+    filter='top', 
+    rownames = FALSE
+)
+
+#'  
+#'  Result table, Samples with less than 0.1% of outliers 
+#' 
+#+ echo=F
+#head(few_outliers_per_sample_23)
+DT::datatable(
+    few_outliers_per_sample_23,
+    filter='top', 
+    rownames = FALSE
+)
+
+saveRDS(few_outliers_per_sample_23, "/s/project/genetic_diagnosis/processed_results/23_outr.Rds")
+
+disp_not_solved_23 <-display_23[display_23$SOLVED!="TRUE",]
+
+#'  
+#'  Samples, which are not solved yet
+#' 
+#+ echo=F
+#head(disp_not_solved_23)
+DT::datatable(
+    disp_not_solved_23,
+    filter='top', 
+    rownames = FALSE
+)
+
 
