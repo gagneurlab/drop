4f251023720999f6d915614f7e254049f12eb28c
Michaela Mueller <mi.mueller@tum.de>
2019-02-27 11:28:15 +0100

    added done file to vep annotation

--- Scripts/Variants/annotate_dt.R
+++ Scripts/Variants/annotate_dt.R
@@ -14,7 +14,7 @@ saveRDS(snakemake, 'tmp/variant_annotation_dt.Rds')
 source("Scripts/_functions/annotation_with_vep.R")
 
 sample <- snakemake@wildcards$vcf
-vcf_obj <- readVcf(vepVcfFile, "hg19")
+vcf_obj <- readVcf(snakemake@input$vcf, "hg19")
 vcf_obj <- vcf_obj[rowRanges(vcf_obj)$QUAL >= minQUAL] # discard any mutations with less minQUAL
 vep_obj <- parseCSQToGRanges(vcf_obj, VCFRowID = rownames(vcf_obj))
 vcf_dt <- combine_vcf_vep(sample, vcf_obj, vep_obj)
