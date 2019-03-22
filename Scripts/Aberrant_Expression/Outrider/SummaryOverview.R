709ebc4beb3dba7ab170f3a5558a92cfc0e41cb1
Vicente <yepez@in.tum.de>
2019-03-22 16:28:11 +0100

    summary overview updated

--- Scripts/Aberrant_Expression/Outrider/SummaryOverview.R
+++ Scripts/Aberrant_Expression/Outrider/SummaryOverview.R
@@ -3,19 +3,17 @@
 #' author: mumichae
 #' wb:
 #'  input:
-#'  - summaries: '`sm expand("Output/html/AberrantExpression/Outrider/{annotation}/OutriderSummary_{dataset}.html", annotation=config["ANNOTATIONS"] , dataset=[*config["outrider"]])`'
+#  - summaries: '`sm expand("Output/html/AberrantExpression/Outrider/{annotation}/OutriderSummary_{dataset}.html", annotation=config["ANNOTATIONS"] , dataset=[*config["outrider_filtered"]])`'
 #' output:
 #'   html_document:
 #'    code_folding: hide
 #'    code_download: TRUE
 #'---
 
-#  - summaries: '`sm expand("Output/html/AberrantExpression/Outrider/{annotation}/OutriderSummary_{dataset}.html", annotation=config["ANNOTATIONS"] , dataset=[*config["outrider_filtered"]])`'
-
 saveRDS(snakemake, "tmp/outrider_overview.snakemake")
 # snakemake <- readRDS("tmp/outrider_overview.snakemake")
 
-summarie_titles <- paste(snakemake@config$ANNOTATIONS, names(snakemake@config$outrider))
+summarie_titles <- paste(snakemake@config$ANNOTATIONS, names(snakemake@config$outrider_filtered))
 summaries <- paste('[', summarie_titles ,'](', gsub(snakemake@config$htmlOutputPath, ".", snakemake@input$summaries), ')', sep = '')
 summaries <- paste(summaries, sep = '\n')
 #' Summaries: `r summaries`
