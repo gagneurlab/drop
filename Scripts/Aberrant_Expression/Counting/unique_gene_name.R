5304705b275ba2438dde5025df0b70ac15a1b8c3
Vicente <yepez@in.tum.de>
2019-05-03 14:09:33 +0200

    unique gene name updated

--- Scripts/Aberrant_Expression/Counting/unique_gene_name.R
+++ Scripts/Aberrant_Expression/Counting/unique_gene_name.R
@@ -3,18 +3,16 @@
 #' author: mumichae, vyepez
 #' wb:
 #'  input:
-#'   - gtex_gene_mapping: "resources/GENCODEv19Mapping.RDS"
-#'   - gtex_txdb: "resources/gencode.v19.genes.patched_contigs.Db"
+#'   - gtex_gtf: "resources/gencode.v19.genes.patched_contigs.gtf.gz" 
 #'   - gencode_gtf: "/s/genomes/human/hg19/gencode29/gencode.v29lift37.annotation.gtf.gz"
 #'  output:
 #'   - gtex_dt: "/s/project/genetic_diagnosis/resource/gencode_v19_unique_gene_name.tsv"
 #'   - gencode_dt: "/s/project/genetic_diagnosis/resource/gencode_v29_unique_gene_name.tsv"
-#'   - gencode_ov_dt: "/s/project/genetic_diagnosis/resource/gencode_v29_overlap_unique_gene_name.tsv"
 #'  type: script
 #'---
 
 saveRDS(snakemake,  "tmp/unique_name.snakemake")
-snakemake <- readRDS("tmp/unique_name.snakemake")
+# snakemake <- readRDS("tmp/unique_name.snakemake")
 suppressPackageStartupMessages({
     library(BSgenome.Hsapiens.UCSC.hg19)
     library(GenomicFeatures)
@@ -25,28 +23,59 @@ suppressPackageStartupMessages({
     library(tidyr)
 })
 
-
-# v19
-gtex_txdb <- loadDb(snakemake@input$gtex_txdb)
-genes_en = sort(genes(gtex_txdb))
-gene_mapping <- readRDS(snakemake@input$gtex_gene_mapping)
-
-genes_dt = as.data.table(genes_en)
-genes_dt = merge(genes_dt, gene_mapping, by = "gene_id")
-
-## We might need to add _2, _3, ... for repeated gene names
-# We can use the following code
-genes_dt[, N := 1:.N, by = gene_name]
-genes_dt[, gene_name_unique := gene_name]
-genes_dt[N > 1, gene_name_unique := paste(gene_name, N, sep = "_")]
-genes_dt[, N := NULL]
-genes_dt[, gene_id_unique := gene_id]
-fwrite(genes_dt, snakemake@output$gtex_dt)
+# Function to obtain number of junctions of each gene
+obtain_junctions <- function(txdb){
+  txdb <- keepStandardChromosomes(txdb)
+  it <- intronicParts(txdb, linked.to.single.gene.only = FALSE)
+  it_dt <- as.data.table(it)
+  it_dt[, gene_id := as.character(gene_id)]
+  junctions_dt <- it_dt[, .(N_junctions = .N), by = gene_id]
+  return(junctions_dt)
+}
 
 
-# v29
-gtf_or <- rtracklayer::import(snakemake@input$gencode_gtf) %>% as.data.table
-gtf_dt <- copy(gtf_or)
+#### v19
+# downloaded from https://storage.googleapis.com/gtex_analysis_v7/reference/gencode.v19.genes.v7.patched_contigs.gtf
+
+## Make txdb object
+gtex_txdb = makeTxDbFromGFF(snakemake@input$gtex_gtf, format='gtf')
+# rename chromosomes from 1 to chr1, ...
+seqlevelsStyle(gtex_txdb) <- "UCSC"
+message(seqlevels(gtex_txdb))
+saveDb(gtex_txdb, "/s/project/genetic_diagnosis/processed_results/v19/txdb.db")
+
+## Make dt with gene names
+gtex_dt <- rtracklayer::import(snakemake@input$gtex_gtf) %>% as.data.table
+gtex_dt <- gtex_dt[type == "transcript", .(seqnames, start, end, width, strand, gene_id, gene_name, gene_type, gene_status)]
+gtex_dt[, seqnames := paste0("chr", seqnames)]
+
+## We need to add _2, _3, ... for repeated gene names
+gtex_dt[, N := 1:.N, by = gene_name]
+gtex_dt[, gene_name_unique := gene_name]
+gtex_dt[N > 1, gene_name_unique := paste(gene_name, N, sep = "_")]
+gtex_dt[, N := NULL]
+gtex_dt[, gene_id_unique := gene_id]
+
+# Add number of junctions
+gtex_junctions <- obtain_junctions(gtex_txdb)
+gtex_dt <- left_join(gtex_dt, gtex_junctions, by = 'gene_id') %>% as.data.table()
+gtex_dt[is.na(N_junctions), N_junctions := 0]
+
+fwrite(gtex_dt, snakemake@output$gtex_dt)
+
+
+#### v29
+
+## Make txdb object
+gencode_txdb = makeTxDbFromGFF(snakemake@input$gencode_gtf, format='gtf')
+# Subset to include only canonical chromosomes
+gencode_txdb <- keepStandardChromosomes(gencode_txdb)
+message('v29')
+message(seqlevels(gencode_txdb))
+saveDb(gencode_txdb, "/s/project/genetic_diagnosis/processed_results/v29/txdb.db")
+
+## Make dt with gene names
+gtf_dt <- rtracklayer::import(snakemake@input$gencode_gtf) %>% as.data.table
 gtf_dt <- gtf_dt[type == "gene", .(seqnames, start, end, strand, gene_id, gene_name, gene_type, gene_status)]
 gtf_dt <- gtf_dt[seqnames %in% GenomeInfoDb::standardChromosomes(BSgenome.Hsapiens.UCSC.hg19)]
 setnames(gtf_dt, "gene_id", "gene_id_unique")
@@ -62,12 +91,11 @@ gtf_dt[, gene_name_unique := gene_name]
 gtf_dt[N > 1, gene_name_unique := paste(gene_name, N, sep = '_')]
 gtf_dt[, N := NULL]
 
-# check if successful
-gtf_dt[gene_name %in% repeated_genes]
-gtf_dt[duplicated(gtf_dt$gene_id)]  # 41 X-Y paralog genes
-dup_genes <- gtf_dt[duplicated(gtf_dt$gene_name), gene_name]
-# View(gtf_dt[gene_name %in% dup_genes])
+# Add number of junctions
+gencode_junctions <- obtain_junctions(gencode_txdb)
+gtf_dt <- left_join(gtf_dt, gencode_junctions, by = c('gene_id_unique' = 'gene_id')) %>% as.data.table()
+gtf_dt[is.na(N_junctions), N_junctions := 0]
 
 fwrite(gtf_dt, snakemake@output$gencode_dt, sep = '\t')
-fwrite(gtf_dt, snakemake@output$gencode_ov_dt, sep = '\t')
+
 
