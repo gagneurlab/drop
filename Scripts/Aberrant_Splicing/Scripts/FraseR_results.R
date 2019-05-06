0e1018d2691901fed290447a02ce94c004274457
Salazar Daniela Soraya Andrade <salazar@in.tum.de>
2019-05-06 17:23:18 +0200

    Update FraseR_results.R

--- Scripts/Aberrant_Splicing/Scripts/FraseR_results.R
+++ Scripts/Aberrant_Splicing/Scripts/FraseR_results.R
@@ -4,7 +4,7 @@
 #' wb:
 #'  input:
 #'   - fds: "/s/project/fraser/snakemake_pipeline/Data/paperPipeline/datasets/savedObjects/ProkischFull/pvaluesBetaBinomial_psiSite.h5"
-#'   - info_script: "../genetic_diagnosis/Scripts/_functions/gene_annotation/add_gene_info_cols.R"
+#'   - info_script: "../../genetic_diagnosis/Scripts/_functions/gene_annotation/add_gene_info_cols.R"
 #'---
 suppressMessages({
     library(ggplot2)
