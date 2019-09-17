3105d4d1bd35b410434ff03809d7ec14253a3032
Michaela Mueller <mi.mueller@tum.de>
2019-09-17 17:12:16 +0200

    non-python files are now within package

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 import setuptools
+import os
+import glob
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+
+extra_files = []
+for (path, directories, filenames) in os.walk('drop/'):
+    directories[:] = [d for d in directories if not (d.startswith('.') or d == 'Data')]
+    filenames[:] = [f for f in filenames if 
+                    not (f.startswith('.') or f.endswith('.Rproj') or f.endswith('.py'))]
+    for filename in filenames:
+        extra_files.append(os.path.join('..', path, filename))
 
 setuptools.setup(
     name="drop",
@@ -12,5 +23,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://i12g-gagneurweb.informatik.tu-muenchen.de/gitlab/salazar/drop.git",
-    packages=setuptools.find_packages(include=["drop", "wBuild", "snakemake"])
+    packages=setuptools.find_packages(include=["drop", "wBuild", "snakemake"]),
+    package_data={'drop': extra_files},
+    include_package_data=True
 )
