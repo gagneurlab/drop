b88207761bd8f47989e50e968aeb6e362d4b7916
Michaela Mueller <mi.mueller@tum.de>
2019-10-13 00:34:36 +0200

    code cleanup

--- drop/submodules.py
+++ drop/submodules.py
@@ -1,13 +1,11 @@
 import os
 import oyaml
 
-def getRoot():
-    return os.path.join(os.getcwd(), ".drop")
-    #return os.path.join(os.path.dirname(__file__), "modules")
-
-methods = {'AE': 'aberrant-expression-pipeline',
+METHODS = {'AE': 'aberrant-expression-pipeline',
            'AS': 'aberrant-splicing-pipeline',
            'MAE': 'mae-pipeline'}
+ROOT = os.path.join(os.getcwd(), ".drop")
+#return os.path.join(os.path.dirname(__file__), "modules")
 
 def getMethodPath(method, link_type='workdir', tmp_dir=None):
     """
@@ -17,15 +15,15 @@ def getMethodPath(method, link_type='workdir', tmp_dir=None):
         config_file: path to config file copy
         final_file: path to empty file used as last output of workflow
     """
-    if method not in methods.keys():
-        raise ValueError(f'{method} is not a valid method. Must be one of {methods.keys()}')
+    if method not in METHODS.keys():
+        raise ValueError(f'{method} is not a valid method. Must be one of {METHODS.keys()}')
     
     if link_type == 'workdir':
-        return os.path.join(getRoot(), methods[method])
+        return os.path.join(ROOT, METHODS[method])
     elif link_type == 'snakefile':
-        return os.path.join(getRoot(), methods[method], 'Snakefile')
+        return os.path.join(ROOT, METHODS[method], 'Snakefile')
     elif link_type == 'rules':
-        return os.path.join(getRoot(), methods[method], 'snakeRules')
+        return os.path.join(ROOT, METHODS[method], 'snakeRules')
     elif link_type == 'config_file':
         assert tmp_dir is not None
         return os.path.join(tmp_dir, f'config_{method}.yaml')
@@ -35,33 +33,30 @@ def getMethodPath(method, link_type='workdir', tmp_dir=None):
     else:
       raise ValueError(f'invalid link_type: "{link_type}"')
 
-def getTmpDir():
-    return os.path.join('.drop', 'tmp')
-
 def setupTempFiles(config):
     
     # create temporary directory
-    tmpdir = getTmpDir()
-    if not os.path.exists(tmpdir):
-        print(f"create temporary files directory {tmpdir}")
-        os.mkdir(tmpdir)
+    TMP_DIR = os.path.join(config['root'], 'tmp')
+    if not os.path.exists(TMP_DIR):
+        print(f"create temporary files directory {TMP_DIR}")
+        os.mkdir(TMP_DIR)
 
     config_files = {}
     done_files = {}
-    for method in methods.keys():
+    for method in METHODS.keys():
         
         # save config files
-        conf_file = getMethodPath(method, link_type = 'config_file', tmp_dir=tmpdir)
+        conf_file = getMethodPath(method, link_type = 'config_file', tmp_dir=TMP_DIR)
         with open(conf_file, 'w') as yaml_file:
             oyaml.dump(config.copy(), yaml_file, default_flow_style=False)
             config_files[method] = conf_file
         
         # final rule output file
-        done_file = getMethodPath(method, link_type='final_file', tmp_dir=tmpdir)
+        done_file = getMethodPath(method, link_type='final_file', tmp_dir=TMP_DIR)
         done_files[method] = done_file
         # remove if it exists
         if os.path.exists(done_file):
             os.remove(done_file)
 
-    return tmpdir, config_files, done_files
+    return TMP_DIR, config_files, done_files
 
