4912ae61f33fa8c4c457027e8612ec43f708d710
Michaela Müller <mumichae@in.tum.de>
2019-11-10 09:34:06 -0500

    moved sample_anno remnaming into download script

--- travis/download_data.sh
+++ travis/download_data.sh
@@ -5,4 +5,7 @@ cd $PROJECT_DIR
 wget -nc "https://i12g-gagneurweb.informatik.tu-muenchen.de/public/paper/drop_analysis/resource.tar.gz"
 tar -zxvf resource.tar.gz
 mv resource Data
+pwd
+ls
+python fix_sample_anno.py
 
