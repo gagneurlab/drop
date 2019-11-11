c5d8f6031a544308ae6004e8c2f75f0695a4e5f1
Michaela Müller <mumichae@in.tum.de>
2019-11-11 06:32:54 -0500

    remove data directory from cache

--- travis/download_data.sh
+++ travis/download_data.sh
@@ -4,8 +4,7 @@ set -e
 cd $PROJECT_DIR
 wget -nc "https://i12g-gagneurweb.informatik.tu-muenchen.de/public/paper/drop_analysis/resource.tar.gz"
 tar -zxvf resource.tar.gz
+rm -rf Data
 mv resource Data
 cd Data
-pwd
-ls
 python fix_sample_anno.py
