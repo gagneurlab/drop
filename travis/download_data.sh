428797db8b416766d0fd89a5ad016d129a220e8d
Michaela Müller <mumichae@in.tum.de>
2019-11-13 07:50:07 -0500

    added before script

--- travis/download_data.sh
+++ travis/download_data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+mkdir -p $PROJECT_DIR
 cd $PROJECT_DIR
 wget -nc "https://i12g-gagneurweb.informatik.tu-muenchen.de/public/paper/drop_analysis/resource.tar.gz"
 tar -zxvf resource.tar.gz
