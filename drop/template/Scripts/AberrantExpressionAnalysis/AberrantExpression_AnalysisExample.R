06c27c2d03af9760c4b331cc7db888f1c1ccc10e
Vicente <yepez@in.tum.de>
2019-11-26 17:39:31 +0100

    aberrant expression analysis scripts modified

---
+++ drop/template/Scripts/AberrantExpressionAnalysis/AberrantExpression_AnalysisExample.R
@@ -0,0 +1,22 @@
+#'---
+#' title: Analysis Example
+#' author: salazar
+#' wb:
+#'  input:
+#'    - indexFile: '`sm parser.getProcDataDir() + "/indexNames.txt"  `' 
+#' output:
+#'   html_document:
+#'    code_folding: hide
+#'    code_download: TRUE
+#'---
+
+
+
+#+ echo=F
+saveRDS(snakemake, paste0(snakemake@config$tmpdir, "/AberrantExpression_Analysis.snakemake") )
+# snakemake <- readRDS(paste0(snakemake@config$tmpdir, "/AberrantExpression.snakemake") )
+
+#' ... Put your results, plots and further analysis here :)
+ 
+
+
---
+++ drop/template/Scripts/AberrantExpressionAnalysis/Counting.R
@@ -0,0 +1,31 @@
+#'---
+#' title: RNA-Seq Counts
+#' author: salazar, mumichae
+#' wb:
+#'  py:
+#'   - |
+#'    annotations = list(config["geneAnnotation"].keys())
+#'    datasets = config["aberrantExpression"]["groups"]
+#'  input:
+#'    - count_files: '`sm expand(parser.getProcDataDir() + "/aberrant_expression/{annotation}/outrider/{dataset}/total_counts.Rds", annotation=annotations, dataset=datasets)`'
+#'    - html: '`sm expand(config["htmlOutputPath"] + "/AberrantExpression/Counting/{annotation}/Summary_{dataset}.html", annotation=annotations, dataset=datasets)`'
+#' output:
+#'   html_document:
+#'    code_folding: hide
+#'    code_download: TRUE
+#'---
+
+#+ echo=F
+saveRDS(snakemake, ".tmp/AberrantExpression_Counting.snakemake")
+# snakemake <- readRDS(".tmp/AberrantExpression_Counting.snakemake")
+
+groups <- names(snakemake@config$outrider_all)
+anno_version <- names(snakemake@config$geneAnnotation)
+html_file_dir <- gsub(snakemake@config$htmlOutputPath, '.', dirname(dirname(snakemake@input$html)))
+summaries_titles <- sapply(anno_version, function(v) {
+        paste0('[', groups ,'](', html_file_dir, '/', v, '/Summary_', groups, '.html){target="_blank"}', collapse = ' ')
+    }
+)
+
+#' ## Count Summaries
+#' `r paste0('Gene annotation version ', names(summaries_titles), ': ', summaries_titles, collapse = '\n')`
