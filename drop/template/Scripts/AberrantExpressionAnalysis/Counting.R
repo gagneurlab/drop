33d7ed2382f5d97d22da264f4a34bbb3252f1ee0
Michaela Müller <mumichae@in.tum.de>
2019-12-05 10:55:37 -0500

    fixed wrong parameters for html links

--- drop/template/Scripts/AberrantExpressionAnalysis/Counting.R
+++ drop/template/Scripts/AberrantExpressionAnalysis/Counting.R
@@ -7,6 +7,8 @@
 #'    annotations = list(config["geneAnnotation"].keys())
 #'    datasets = config["aberrantExpression"]["groups"]
 #'  params:
+#'    - annotations: '`sm annotations`'
+#'    - datasets: '`sm datasets`'
 #'    - tmpdir: '`sm drop.getTmpDir()`'
 #'    - count_files: '`sm expand(parser.getProcDataDir() +
 #'                    "/aberrant_expression/{annotation}/outrider/{dataset}/total_counts.Rds",
@@ -26,13 +28,18 @@
 saveRDS(snakemake, file.path(snakemake@params$tmpdir, "AberrantExpression_Counting.snakemake"))
 # snakemake <- readRDS(".drop/tmp/AberrantExpression_Counting.snakemake")
 
-groups <- names(snakemake@config$outrider_all)
-anno_version <- names(snakemake@config$geneAnnotation)
-html_file_dir <- gsub(snakemake@config$htmlOutputPath, '.', dirname(dirname(snakemake@params$html)))
+groups <- snakemake@params$datasets
+anno_version <- snakemake@params$annotations
+html_file_dir <- gsub(snakemake@config$htmlOutputPath, '.', 
+                      dirname(dirname(snakemake@params$html)))
 summaries_titles <- sapply(anno_version, function(v) {
-        paste0('[', groups ,'](', html_file_dir, '/', v, '/Summary_', groups, '.html){target="_blank"}', collapse = ' ')
+        paste0('[', groups ,'](', html_file_dir, '/',
+               v, '/Summary_', groups, '.html){target="_blank"}', 
+              collapse = ' ')
     }
 )
 
 #' ## Count Summaries
-#' `r paste0('Gene annotation version ', names(summaries_titles), ': ', summaries_titles, collapse = '\n')`
+#' `r paste0('Gene annotation version ', names(summaries_titles),
+#'           ': ', summaries_titles, collapse = '\n')`
+
