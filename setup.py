186c088ab485275724bf8fba1ada268adddff948
Michaela Müller <mi.mueller@tum.de>
2020-07-20 04:10:16 +1000

    added python dependencies to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,14 @@ import glob
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+requirements = [
+    'wbuild>=1.7.0',
+    'python-dateutil',
+    'pandoc',
+    'graphviz',
+    'pandas>=0.13',
+]
+
 extra_files = []
 for (path, directories, filenames) in os.walk('drop/'):
     directories[:] = [d for d in directories if not (d.startswith('.') or d == 'Data')]
@@ -25,7 +33,8 @@ setuptools.setup(
     packages=setuptools.find_packages(include=["drop"]),
     entry_points={'console_scripts': ['drop=drop.cli:main']},
     package_data={'drop': extra_files},
-    include_package_data=True
+    include_package_data=True,
+    install_requires=requirements
 )
 
 
