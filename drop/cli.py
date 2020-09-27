7ecfaf15432ade3784720c88ff7a65111319b912
mumichae <mi.mueller@tum.de>
2020-09-27 18:43:20 +1000

    updated version number in README and drop/cli

--- drop/cli.py
+++ drop/cli.py
@@ -13,7 +13,7 @@ click_log.basic_config(logger)
 
 @click.group()
 @click_log.simple_verbosity_option(logger)
-@click.version_option('0.9.1',prog_name='drop')
+@click.version_option('0.9.2',prog_name='drop')
 def main():
     pass
 
