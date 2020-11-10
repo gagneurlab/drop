c87853c82bb2bfcbb75aa6c8132ff55c51e846f3
Vicente <yepez@in.tum.de>
2020-11-10 13:27:09 +0100

    doc hpoFile, files to download

--- drop/config/DropConfig.py
+++ drop/config/DropConfig.py
@@ -11,7 +11,7 @@ class DropConfig:
         # wbuild keys
         "projectTitle", "htmlOutputPath", "scriptsPath", "indexWithFolderName", "fileRegex", "readmePath",
         # global parameters
-        "root", "sampleAnnotation", "geneAnnotation", "genomeAssembly", "exportCounts", "tools",
+        "root", "sampleAnnotation", "geneAnnotation", "genomeAssembly", "exportCounts", "tools", "hpoFile",
         # modules
         "aberrantExpression", "aberrantSplicing", "mae"
     ]
@@ -94,7 +94,7 @@ class DropConfig:
         setKey = utils.setKey
         setKey(config_dict, None, "fileRegex", ".*\.(R|md)")
         setKey(config_dict, None, "genomeAssembly", "hg19")
-        # setKey(config_dict, None, "hpoFile", NULL)
+        setKey(config_dict, None, "hpoFile", None)
 
         # set submodule dictionaries
         setKey(config_dict, None, "aberrantExpression", dict())
