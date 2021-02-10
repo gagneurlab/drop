2cce9ae6fd9925a3704b85d80962a98b76032f5b
Christian Mertes <mertes@in.tum.de>
2021-02-10 07:58:23 +0000

    fix download and add more test cases

--- drop/download_data.sh
+++ drop/download_data.sh
@@ -4,7 +4,7 @@ set -e
 # get data
 resource_url="https://www.cmm.in.tum.de/public/paper/drop_analysis/resource_splice_merge.tar.gz"
 tmpdir="$(dirname "$(tempfile)")"
-wget -nc -P $tmpdir $resource_url
+wget -nc -O $tmpdir/resource.tar.gz $resource_url
 mkdir -p Data
 if [ -z "$(ls Data)" ]; then
 	tar -zxvf "$tmpdir/resource.tar.gz" -C .
