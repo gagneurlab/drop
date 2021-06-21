19e9b3d68927ad9a71a4f1e258a4059f5fcfeb9f
Michaela Mueller <mumichae@in.tum.de>
2021-06-21 23:32:03 +0200

    renamed getSnakefile

--- drop/config/submodules/Submodules.py
+++ drop/config/submodules/Submodules.py
@@ -4,6 +4,7 @@ import os
 from pathlib import Path
 from wbuild.createIndex import createIndexRule
 
+
 class Submodule:
 
     def __init__(self, config, sampleAnnotation, processedDataDir, processedResultsDir, workDir):
@@ -26,11 +27,17 @@ class Submodule:
             raise KeyError(f"{key} not defined for {self.name} config")
         return self.dict_[key]
 
-    def getWorkdir(self, hide_dir = False, str_=True):
+    def getWorkdir(self, hide_dir=False, str_=True):
         if hide_dir:
             return utils.returnPath(self.workDir / "Scripts" / ("_" + self.name) / "pipeline", str_)
         else:
             return utils.returnPath(self.workDir / "Scripts" / self.name / "pipeline", str_)
+
+    def getSnakefile(self):
+        if os.path.isdir(self.getWorkdir()):
+            return self.getWorkdir() + "/Snakefile"
+        else:
+            return self.getWorkdir(hide_dir=True) + "/Snakefile"
 
     def checkSubset(self, groupSubsets, warn=30, error=10):
         """
@@ -48,46 +55,38 @@ class Submodule:
             elif len(groupSubsets[group]) < warn:
                 logger.info(f'WARNING: Less than {warn} IDs in DROP_GROUP {group}')
 
-
-    def find_snakefile(self):
-        if os.path.isdir(self.getWorkdir()):
-            return self.getWorkdir() + "/Snakefile"
-        else:
-            return self.getWorkdir(hide_dir = True) + "/Snakefile"
-
-
     def renameLocalDir(self):
-        work_dir_prefix = utils.returnPath(self.workDir / "Scripts",False)
+        work_dir_prefix = utils.returnPath(self.workDir / "Scripts", False)
         work_dir_off = work_dir_prefix / ("_" + self.name)
         work_dir_on = work_dir_prefix / self.name
 
         if self.run:
             work_dir = Path(work_dir_on / "pipeline")
-            if(os.path.isdir(work_dir_off)):
+            if (os.path.isdir(work_dir_off)):
                 try:
-                    os.rename(work_dir_off,work_dir_on)
+                    os.rename(work_dir_off, work_dir_on)
                 except:
                     raise OSError(f"Could not rename {work_dir_off} to {work_dir_on}\n \
                         try running 'drop update' to reset file structure\n")
         else:
             work_dir = Path(work_dir_off / "pipeline")
-            if(os.path.isdir(work_dir_on)):
+            if (os.path.isdir(work_dir_on)):
                 try:
-                    os.rename(work_dir_on,work_dir_off)
+                    os.rename(work_dir_on, work_dir_off)
                 except:
                     raise OSError(f"Could not rename {work_dir_on} to {work_dir_off}\n \
                         try running 'drop update' to reset file structure\n")
 
         return work_dir
 
-    def getModuleIndexFiles(self,index_name,work_dir):
+    def getModuleIndexFiles(self, index_name, work_dir):
         run = self.run
-        if(run):
+        if (run):
             index_input, index_output, graph_file, _ = createIndexRule(
                 scriptsPath=str(work_dir),
-                index_name= index_name
+                index_name=index_name
             )
-            return index_input,graph_file,index_output
+            return index_input, graph_file, index_output
         else:
-            #dummy return of hidden pipeline directory which already exists
-            return self.getWorkdir(hide_dir = True),self.getWorkdir(hide_dir = True),self.getWorkdir(hide_dir = True)
+            # dummy return of hidden pipeline directory which already exists
+            return self.getWorkdir(hide_dir=True), self.getWorkdir(hide_dir=True), self.getWorkdir(hide_dir=True)
