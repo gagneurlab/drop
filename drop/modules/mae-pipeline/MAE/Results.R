c4c7f0c0dbf6ebbb0a7a68e7d7d54d847256983e
Nick <smithnickh@gmail.com>
2021-06-24 08:47:23 -0700

    change formatting for MAE results

--- drop/modules/mae-pipeline/MAE/Results.R
+++ drop/modules/mae-pipeline/MAE/Results.R
@@ -134,6 +134,8 @@ viewing_cutoff <- 1000
 if (nrow(res) < viewing_cutoff){
     viewing_cutoff <- nrow(res)
 }
+
+
 cat(paste0("<p> Taking a look at no more than the first 1,000 results.<p>"))
 DT::datatable(res[MAE_ALT == TRUE][1:viewing_cutoff], filter = 'top')
 
