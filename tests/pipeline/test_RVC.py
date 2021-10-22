d9b93175583ec5518021011a39b17d95ab1f49ae
Smith Nicholas <smith@in.tum.de>
2021-10-22 14:05:44 +0200

    add batch_1. add full path to test

--- tests/pipeline/test_RVC.py
+++ tests/pipeline/test_RVC.py
@@ -27,8 +27,8 @@ class Test_RVC_Pipeline:
     @pytest.mark.usefixtures("pipeline_run")
     # count the number of variant calls in batch0 before splitting
     def test_variants_joint(self, demo_dir):
-        vcf_file0 = "Output/processed_data/rnaVariantCalling/out/all_samples_haplocaller/batch_0_all_samples.genotyped.vcf.gz"
-        vcf_file1 = "Output/processed_data/rnaVariantCalling/out/all_samples_haplocaller/batch_1_all_samples.genotyped.vcf.gz"
+        vcf_file0 = f"{demo_dir}/Output/processed_data/rnaVariantCalling/out/all_samples_haplocaller/batch_0_all_samples.genotyped.vcf.gz"
+        vcf_file1 = f"{demo_dir}/Output/processed_data/rnaVariantCalling/out/all_samples_haplocaller/batch_1_all_samples.genotyped.vcf.gz"
         r_cmd = """ 
                 library(data.table)
                 vcf0  <- fread("{}")
@@ -46,8 +46,8 @@ class Test_RVC_Pipeline:
     @pytest.mark.usefixtures("pipeline_run")
     # count the number of variant calls in batch0 before splitting
     def test_variants_single_line_multi(self, demo_dir):
-        vcf_file0 = "Output/processed_data/rnaVariantCalling/out/all_samples_haplocaller/batch_0_all_samples.genotyped.filtered_clean.vcf.gz"
-        vcf_file1 = "Output/processed_data/rnaVariantCalling/out/all_samples_haplocaller/batch_1_all_samples.genotyped.filtered_clean.vcf.gz"
+        vcf_file0 = f"{demo_dir}/Output/processed_data/rnaVariantCalling/out/all_samples_haplocaller/batch_0_all_samples.genotyped.filtered_clean.vcf.gz"
+        vcf_file1 = f"{demo_dir}/Output/processed_data/rnaVariantCalling/out/all_samples_haplocaller/batch_1_all_samples.genotyped.filtered_clean.vcf.gz"
         r_cmd = """ 
                 library(data.table)
                 vcf0  <- fread("{}")
@@ -63,7 +63,7 @@ class Test_RVC_Pipeline:
 
     @pytest.mark.usefixtures("pipeline_run")
     def test_single_sample_variants(self, demo_dir):
-        result_dir = "Output/processed_data/rnaVariantCalling/out/sample_haplocaller/HG00096"
+        result_dir = f"{demo_dir}/Output/processed_data/rnaVariantCalling/out/sample_haplocaller/HG00096"
         r_cmd = """ 
                 library(data.table)
                 masked_filter <- fread(file.path("{}", "HG00096.genotyped.filtered.basic10.masked.vcf.gz"))
