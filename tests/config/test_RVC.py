b38ddaf8fac19dd63ea5d3187766d195ddbfefc8
Nick <smithnickh@gmail.com>
2021-11-05 14:37:31 +0100

    rename RVC demo_files

--- tests/config/test_RVC.py
+++ tests/config/test_RVC.py
@@ -3,8 +3,8 @@ class Test_RVC_Config:
         assert dropConfig.RVC.getWorkdir() == demo_dir + "/Scripts/rnaVariantCalling/pipeline"
         dict_ = {
             'groups': ['batch_0','batch_1'],
-            'knownVCFs': [f'{demo_dir}/Data/high_confidence_snps_test.vcf.gz', f'{demo_dir}/Data/high_confidence_indels_test.vcf.gz', f'{demo_dir}/Data/dbSNP_chr21_test.vcf.gz'],
-            'repeat_mask': f'{demo_dir}/Data/repeat_mask_chr21_test.bed',
+            'knownVCFs': [f'{demo_dir}/Data/high_confidence_snps.vcf.gz', f'{demo_dir}/Data/high_confidence_indels.vcf.gz', f'{demo_dir}/Data/dbSNP_chr21.vcf.gz'],
+            'repeat_mask': f'{demo_dir}/Data/repeat_mask_chr21.bed',
             'hcArgs': '',
             'minAlt': 10
         }
