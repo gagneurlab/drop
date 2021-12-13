e11e5fd858235ec2d9db314b03ccf1b50e541ead
Vicente <yepez@in.tum.de>
2021-12-13 18:20:18 +0100

    hpo function bug fixed

--- drop/modules/helpers/add_HPO_cols.R
+++ drop/modules/helpers/add_HPO_cols.R
@@ -14,12 +14,13 @@ add_HPO_cols <- function(RES, sample_id_col = 'sampleID',
   # change column names
   setnames(RES, old = c(sample_id_col, gene_name_col), new = c('sampleID', 'hgncSymbol'))
   
+  # Get HPO terms of all genes
   f2 <- merge(RES[, .(sampleID, hgncSymbol)], 
             hpo_dt[,.(hgncSymbol, HPO_id, HPO_label)], by = 'hgncSymbol')
   
   if(nrow(f2) > 0){
     f3 <- merge(f2, sa[,.(RNA_ID, HPO_TERMS)], by.x = 'sampleID', by.y = 'RNA_ID')
-    f3[, HPO_match := HPO_id %in% unlist(strsplit(HPO_TERMS, split = ','))]
+    f3[, HPO_match := HPO_id %in% unlist(strsplit(HPO_TERMS, split = ',')), by = 1:nrow(f3)]
     f3 <- f3[HPO_match == TRUE]
     if(nrow(f3) > 0){
       f4 <- f3[, .(HPO_id_overlap = paste(HPO_id, collapse = ', '), 
