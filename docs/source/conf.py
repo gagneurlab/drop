8989d63574f25443b29e1803179ed2f15760847e
Smith Nicholas <smith@in.tum.de>
2022-03-03 19:03:32 +0100

    bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Michaela Müller'
 author = 'Michaela Müller'
 
 # The full version, including alpha/beta/rc tags
-release_ = '1.1.2'
+release_ = '1.1.3'
 
 
 # -- General configuration ---------------------------------------------------
