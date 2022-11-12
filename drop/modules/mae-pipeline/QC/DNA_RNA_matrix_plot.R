6da22cbb687b0e101a1d8f647639b5d66fb22e71
Vicente Yepez <30469316+vyepez88@users.noreply.github.com>
2022-11-12 08:01:09 +0100

    Qc heatmap (#390)
    * dna rna heatmap added
    
    * dna rna heatmap added
    
    * x label heatmap fixed
    
    * x label heatmap fixed
    
    * results display simplified
    
    * fix no results scenario in overview
    
    * results display simplified
    
    * results display simplified
    
    Co-authored-by: Vicente <yepez@in.tum.de>
    Co-authored-by: nickhsmith <smithnickh@gmail.com>

--- drop/modules/mae-pipeline/QC/DNA_RNA_matrix_plot.R
+++ drop/modules/mae-pipeline/QC/DNA_RNA_matrix_plot.R
@@ -96,8 +96,8 @@ ann_colors[['status']] <- ann_colors[['status']][unique(c(dna_df$status, rna_df$
 
 #+ Heatmap, fig.height=6, fig.width=8
 pheatmap(qc_mat, color = color, cluster_rows = FALSE, cluster_cols = FALSE, 
-         annotation_row = dna_df, annotation_col = rna_df, annotation_colors = ann_colors, 
-         labels_row = 'DNA samples', labels_col = 'RNA samples')
+         annotation_row = dna_df, annotation_col = rna_df, annotation_colors = ann_colors,
+         labels_row = 'DNA samples', labels_col = 'RNA samples', angle_col = 0)
 
 
 #' ## Identify matching samples
