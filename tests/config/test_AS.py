d06095548c014f1691acd0902f2f589f92961512
AtaJadidAhari <atajadidahari@gmail.com>
2024-10-07 17:16:53 +0200

    adjust tests for FRASER2

--- tests/config/test_AS.py
+++ tests/config/test_AS.py
@@ -18,7 +18,7 @@ class Test_AS_Config:
             'deltaPsiCutoff': 0.05,
             'maxTestedDimensionProportion': 6,
             'genesToTest': 'Data/genes_to_test.yaml',
-            'FRASER_version': 'FRASER'
+            'FRASER_version': 'FRASER2',
         }
         assert dict_.items() <= dropConfig.AS.dict_.items()
 
