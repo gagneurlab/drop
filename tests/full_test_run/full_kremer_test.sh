ed87599bb86b51763e4176eef9803416decf0d50
Christian Mertes <mertes@in.tum.de>
2026-03-16 11:46:14 +0100

    align test with Kremer et al data

--- tests/full_test_run/full_kremer_test.sh
+++ tests/full_test_run/full_kremer_test.sh
@@ -1,42 +1,59 @@
 #!/bin/bash
-
+#
+# Script to run a full fletched analysis on our wellknown Kremer et. al. data
+# This ensures, that we can reidentify all known cases after each update.
+#
+# This full test should be run at every release and should not throw an error.
+#
+script_dir=$(realpath $(dirname $0))
 
 # Requires a server with 1Tb RAM!!!
-# Use max 120 CPUs / threads in parallel
-num_cores=120
-
-# Define the branch that needs to be tested
-branch_name=dev
-
-# create test env first
-env_path=./drop_test_env
-mamba create -p ${env_path} "drop>=1.5.0" "pandoc>2.4" "r-base>=4.5"  
-conda activate ${env_path}
-
-# install new version from git branch 
-pip install git+https://github.com/gagneurlab/drop@${branch_name}
+# Use max 200 CPUs / threads in parallel
+num_cores=${1:-250}
+branch_name=${2:-dev}
+test_dir_root=${3:-$TMP}
 
 # create fresh folder to run drop
-run_folder=run_drop_${RANDOM}
+run_folder=${test_dir_root}/drop-test/run_drop_${RANDOM}
 mkdir -p ${run_folder}
 cd ${run_folder}
 
+# create test env first
+env_path=./env/drop_test_env
+mkdir -p env
+mamba create -y -p ${env_path} \
+    "drop>=1.5" "pandoc>=2.4" "r-base>=4.4" \
+    "python>=3.12" "pip>=26.0" "yq>=3.4" \
+    "pandas>=3.0"
+conda activate ${env_path}
+
+# install version from git branch
+pip install --no-deps --force-reinstall \
+    "git+https://github.com/gagneurlab/drop@${branch_name}"
+
 # init drop and config
 drop init
-sed -re 's|^(root:).*|\1 "./"|' -i config.yaml
-sed -re 's|^(htmlOutputPath:).*|\1 "./html"|' -i config.yaml
-sed -re 's|^(sampleAnnotation:).*|\1 "/s/project/drop-analysis/kremer_checks/sample_annotation.tsv"|' -i config.yaml
-sed -re 's|^(    v29:).*|\1 "/s/genomes/human/hg19/gencode29/gencode.v29lift37.sorted.gtf.gz"|' -i config.yaml
-sed -re 's|^(genome:).*|\1 \n    ucsc: "/s/genomes/human/hg19/fasta/hg19.fa"|' -i config.yaml
-sed -re 's|^(    qcVcf:).*|\1 "/s/public_webshare/public/paper/drop_analysis/resource/qc_vcf_1000G_hg19.vcf.gz"|' -i config.yaml
+
+# adapt config to local setup
+yq -yi '
+  .root = "" |
+  .htmlOutputPath = "html" |
+  .sampleAnnotation = "/s/project/drop-analysis/kremer_checks/sample_annotation_80.tsv" |
+  .geneAnnotation.v29 = "/s/genomes/human/hg19/gencode29/gencode.v29lift37.sorted.gtf.gz" |
+  .genome.ucsc = "/s/genomes/human/hg19/fasta/hg19.fa" |
+  .mae.groups = ["mae"] |
+  .mae.qcVcf = "/s/public_webshare/public/paper/drop_analysis/resource/qc_vcf_1000G_hg19.vcf.gz" |
+  .rnaVariantCalling = {} |
+  .rnaVariantCalling.run = false
+  ' config.yaml
 
 # run drop locally for all samples and modules
-snakemake -j ${num_cores} -n \
+snakemake -j 1 -n
+snakemake --jobs ${num_cores} \
     --rerun-triggers mtime \
-    --default-resources ntasks=1 mem_mb=5000 gpu=0 \
-    --jobs $num_cores
+    --keep-going \
+    --retries 3
 
-# run final checks on the results
-Rscript ../check_kremer_full_drop.R
-
+# Run final check if we can recall all our findings
+Rscript ${script_dir}/kremer_test.R
 
--- tests/full_test_run/kremer_test.R
+++ tests/full_test_run/kremer_test.R
@@ -1,40 +1,62 @@
-## Parameters to change
+#!/usr/env Rscript
+#
+# Rscript to test that we can recall all the known outliers and events
+# in the Kremer et. al. dataset after a successful DROP run.
+#
 
-root_dir <- '/s/project/drop-analysis/kremer_checks/run_drop_26267' # adjust
+## Parameters to change
+library(data.table)
+library(OUTRIDER)
+library(FRASER)
+library(yaml)
+
+# default groups and folders
 root_dir <- './'
-drop_group <- 'kremer' # adjust
+outrider_drop_group1 <- 'group1'
+outrider_drop_group2 <- 'group2'
+fraser_drop_group <- 'fraser'
+mae_drop_group <- 'mae'
+
+## Sample Annotation
+sample_anno <- fread(read_yaml(file.path(root_dir, "config.yaml"))$sampleAnnotation)
 
 ## OUTRIDER 
-counts_out <- readRDS(file.path(root_dir, 'processed_data/aberrant_expression/v29/outrider', drop_group, 'total_counts.Rds'))
-granges_out <- readRDS(file.path(root_dir, 'processed_data/preprocess/v29/count_ranges.Rds')) 
-res_out <- fread(file.path(root_dir, 'processed_results/aberrant_expression/v29/outrider', drop_group, 'OUTRIDER_results.tsv'))
-ods <- readRDS(file.path(root_dir, 'processed_results/aberrant_expression/v29/outrider', drop_group, 'ods.Rds'))
+counts_out <- readRDS(file.path(root_dir, 'processed_data/aberrant_expression/v29/outrider', outrider_drop_group2, 'total_counts.Rds'))
+granges_out <- readRDS(file.path(root_dir, 'processed_data/aberrant_expression/v29/count_ranges.Rds'))
+res_out <- fread(file.path(root_dir, 'processed_results/aberrant_expression/v29/outrider', outrider_drop_group2, 'OUTRIDER_results.tsv'))
+ods <- readRDS(file.path(root_dir, 'processed_results/aberrant_expression/v29/outrider', outrider_drop_group2, 'ods.Rds'))
+ods1 <- readRDS(file.path(root_dir, 'processed_results/aberrant_expression/v29/outrider', outrider_drop_group1, 'ods.Rds'))
+
 
 # FRASER 
-fds_raw <- loadFraserDataSet(file = file.path(root_dir, 'processed_data/aberrant_splicing/datasets/savedObjects', paste0('raw-', drop_group), 'fds-object.RDS'))
-fds <- loadFraserDataSet(file = file.path(root_dir, 'processed_results/aberrant_splicing/datasets/savedObjects', paste0(drop_group, '--v29'), 'fds-object.RDS'))
-res_fra <- fread(file.path(root_dir, 'processed_results/aberrant_splicing/results/v29/fraser', drop_group, 'results.tsv'))
+fds_raw <- loadFraserDataSet(file = file.path(root_dir, 'processed_data/aberrant_splicing/datasets/savedObjects', paste0('raw-', fraser_drop_group), 'fds-object.RDS'))
+fds <- loadFraserDataSet(file = file.path(root_dir, 'processed_results/aberrant_splicing/datasets/savedObjects', paste0(fraser_drop_group, '--v29'), 'fds-object.RDS'))
+res_fra <- fread(file.path(root_dir, 'processed_results/aberrant_splicing/results/v29/fraser', fraser_drop_group, 'results.tsv'))
+
 
 # MAE
-mae <- fread(file.path(root_dir, 'processed_results/mae', drop_group, 'MAE_results_v29.tsv'))
-qc_mat <- readRDS(file.path(root_dir, 'processed_results/mae', drop_group, 'dna_rna_qc_matrix.Rds'))
-
+mae <- fread(file.path(root_dir, 'processed_results/mae', mae_drop_group, 'MAE_results_v29.tsv'))
+qc_mat <- readRDS(file.path(root_dir, 'processed_results/mae', mae_drop_group, 'dna_rna_qc_matrix.Rds'))
 
 
 ### Checks
+##
+## OUTRIDER
+##
 
 # 1. Number of samples & genes. Use gencode29. Count unstranded.
 dim(counts_out)
-stopifnot(dim(counts_out) == c(length(granges_out), 119))
+stopifnot(dim(counts_out) == c(length(granges_out), sum(sample_anno[, grepl(outrider_drop_group2, DROP_GROUP)])))
+stopifnot(ncol(ods1) == sum(sample_anno[, grepl(outrider_drop_group1, DROP_GROUP)]))
 
 # 2. At least 10K expressed genes
-stopifnot(nrow(ods) > 10e3)
+stopifnot(nrow(ods) > 10e3 && nrow(ods) < 20e3)
 
 # 3. TIMMDC1 is expression outlier in both samples & one of the pvalues
 res_out[hgncSymbol == 'TIMMDC1'] # Should give 2 outliers, MUC1365 & MUC1344
 stopifnot(identical(sort(res_out[hgncSymbol == 'TIMMDC1', sampleID]), c('MUC1344', 'MUC1365')))
-stopifnot(assays(ods)$pValue["ENSG00000113845.9_2", "MUC1344"] == 
-            res_out[geneID == 'ENSG00000113845.9_2' & sampleID == "MUC1344", pValue])
+stopifnot(all.equal(assays(ods)$pValue["ENSG00000113845.9_2", "MUC1344"],
+            res_out[geneID == 'ENSG00000113845.9_2' & sampleID == "MUC1344", pValue]))
 
 # 4. MGST1 outlier
 res_out[hgncSymbol == 'MGST1'] # Should be MUC1396
@@ -46,10 +68,22 @@ stopifnot(res_out[hgncSymbol == 'TIMMDC1' & sampleID == 'MUC1344', rawcounts] ==
 # 6. Check counts in one MGST1 sample
 stopifnot(res_out[hgncSymbol == 'MGST1' & sampleID == 'MUC1396', rawcounts] == 366)
 
+# 6a. check result dimensions
+stopifnot(all(sapply(c("MUC1344", "MUC1396", "MUC1365"), function(x) nrow(res_out[sampleID == x])) < c(10, 15, 15)))
+
+
+### Checks
+##
+## FRASER
+##
 
 # 7. FRASER objects Dimensions
 stopifnot(nrow(fds_raw) > 2e6) # Raw FRASER has > 2M rows
-stopifnot(nrow(fds) > 1e5 & nrow(fds) < 5e5) # filtered FRASER has between 100-500K rows
+stopifnot(nrow(fds) > 85000 & nrow(fds) < 2e5) # filtered FRASER has between 80-200K rows for 80 samples
+stopifnot(ncol(fds) == sum(sample_anno[, grepl(fraser_drop_group, DROP_GROUP)]))
+
+# 7a. check result dimensions per sample
+stopifnot(all(sapply(c("MUC1344", "MUC1350", "MUC1365"), function(x) nrow(res_fra[sampleID == x])) < c(15, 10, 25)))
 
 # 8. TIMMDC1 outlier
 stopifnot(identical(sort(res_fra[hgncSymbol == 'TIMMDC1', sampleID]), c('MUC1344', 'MUC1365')))
@@ -58,16 +92,33 @@ stopifnot(identical(sort(res_fra[hgncSymbol == 'TIMMDC1', sampleID]), c('MUC1344
 stopifnot(identical(sort(res_fra[hgncSymbol == 'CLPP', sampleID]), c('MUC1350')))
 
 # 10. Split counts
-stopifnot(res_fra[hgncSymbol == 'CLPP' & sampleID == 'MUC1350', counts] == 40) # if this is the junction chr19 6366375 6368548  2174
+stopifnot(res_fra[hgncSymbol == 'CLPP' & sampleID == 'MUC1350', counts] == 36) # junction chr19 6366375 6368548  2174
+stopifnot(res_fra[hgncSymbol == 'TIMMDC1' & sampleID == 'MUC1344', counts] == 37) # junction chr3 119234787 119236051  1265
+
+
+### Checks
+##
+## MAE
+##
 
 # 11. MAE
-stopifnot(grepl('MUC1404', mae[gene_name == 'ALDH18A1', ID]))
+stopifnot(nrow(mae[gene_name == 'ALDH18A1' & ID == 'MUC1404']) == 1)
 
 # 12. MAE counts
 stopifnot(mae[gene_name == 'ALDH18A1' & ID == "65990--MUC1404", altCount] == 159)
 
-# 13. DNA-RNA QC
+# 13. DNA-RNA QC report any mismatch with the annotation
 limit <- .75
-stopifnot(all(qc_mat[lower.tri(qc_mat)] < limit))
-stopifnot(all(diag(qc_mat) > limit))
+qc_matches <- which(qc_mat > limit, arr.ind=TRUE)
+qc_match_anno <- sapply(seq_len(nrow(qc_matches)), function(x){
+    rna_id = colnames(qc_mat)[qc_matches[x,'col']]
+    dna_id = rownames(qc_mat)[qc_matches[x,'row']]
+    ans = nrow(sample_anno[RNA_ID == rna_id & DNA_ID == dna_id]) == 1
+    if(! ans){
+        message("RNA_ID does not match DNA_ID:\t", rna_id, "\t", dna_id)
+    }
+    ans
+})
+stopifnot(qc_match_anno)
+
 
